3d6c5e53ab42151c0a350d7cc1bdc752c13ae986
Steve Yen <steve.yen@gmail.com>
2013-06-04 19:25:27 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# go-slab - slab allocator in go
+
+For byte array memory management in the Go Programming Language via a
+simple slab allocator algorithm.
+
+See: http://en.wikipedia.org/wiki/Slab_allocation
+
+# LICENSE
+
+Apache 2 license.
---
+++ slab.go
@@ -0,0 +1,212 @@
+//  Copyright (c) 2013 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the
+//  License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an "AS
+//  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+
+package slab
+
+import (
+	"encoding/binary"
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+type slabArena struct {
+	growthFactor float64
+	slabClasses  []slabClass // The chunkSizes of slabClasses grows by growthFactor.
+	slabMagic    int32       // Magic number at the end of each slab memory []byte.
+	slabSize     int
+}
+
+type slabClass struct {
+	slabs     []*slab  // A growing array of slabs.
+	chunkSize int      // Each slab is sliced into fixed-sized chunks.
+	chunkFree chunkLoc // Chunks are tracked in a free-list per slabClass.
+}
+
+type slab struct {
+	memory []byte  // len(memory) == slabSize + SLAB_MEMORY_FOOTER_LEN.
+	chunks []chunk // Parallel array of chunk metadata.
+}
+
+const SLAB_MEMORY_FOOTER_LEN int = 4 + 4 + 4 // slabClassIndex + slabIndex + slabMagic.
+
+type chunkLoc struct {
+	slabIndex  int
+	chunkIndex int
+}
+
+var empty_chunkLoc chunkLoc = chunkLoc{ // A sentinel.
+	slabIndex:  -1,
+	chunkIndex: -1,
+}
+
+func (cl *chunkLoc) isEmpty() bool {
+	return cl.slabIndex == -1 && cl.chunkIndex == -1
+}
+
+type chunk struct {
+	refs int32
+	self chunkLoc
+	next chunkLoc
+}
+
+// Returns an Arena based on an unsynchronized slab allocator implementation.
+func NewSlabArena(startChunkSize int, slabSize int, growthFactor float64) Arena {
+	s := &slabArena{
+		growthFactor: growthFactor,
+		slabClasses:  make([]slabClass, 0, 8),
+		slabMagic:    rand.Int31(),
+		slabSize:     slabSize,
+	}
+	s.addSlabClass(startChunkSize)
+	return s
+}
+
+func (s *slabArena) Alloc(bufSize int) (buf []byte) {
+	if bufSize > s.slabSize {
+		return nil
+	}
+	return s.allocSlabClassChunkMem(s.findSlabClassIndex(bufSize))[0:bufSize]
+}
+
+func (s *slabArena) AddRef(buf []byte) {
+	_, c := s.bufContainer(buf)
+	c.refs++
+	if c.refs <= 1 {
+		panic(fmt.Sprintf("unexpected ref-count during AddRef: %#v", c))
+	}
+}
+
+func (s *slabArena) DecRef(buf []byte) {
+	sc, c := s.bufContainer(buf)
+	c.refs--
+	if c.refs < 0 {
+		panic(fmt.Sprintf("unexpected ref-count during DecRef: %#v", c))
+	}
+	if c.refs == 0 {
+		sc.pushFreeChunk(c)
+	}
+}
+
+func (s *slabArena) addSlabClass(chunkSize int) {
+	s.slabClasses = append(s.slabClasses,
+		s.makeSlabClass(len(s.slabClasses), chunkSize))
+}
+
+func (s *slabArena) makeSlabClass(slabClassIndex, chunkSize int) slabClass {
+	if chunkSize <= 0 {
+		panic(fmt.Sprintf("bad chunkSize: %v", chunkSize))
+	}
+	return slabClass{
+		slabs:     make([]*slab, 0, 16),
+		chunkSize: chunkSize,
+		chunkFree: empty_chunkLoc,
+	}
+}
+
+func (s *slabArena) findSlabClassIndex(bufSize int) int {
+	curr := 0
+	for {
+		slabClass := &(s.slabClasses[curr])
+		if bufSize <= slabClass.chunkSize {
+			return curr
+		}
+		if curr+1 >= len(s.slabClasses) {
+			nextChunkSize := float64(slabClass.chunkSize) * s.growthFactor
+			s.addSlabClass(int(math.Ceil(nextChunkSize)))
+		}
+		curr++
+	}
+}
+
+func (s *slabArena) allocSlabClassChunkMem(slabClassIndex int) (chunkMem []byte) {
+	sc := &(s.slabClasses[slabClassIndex])
+	if sc.chunkFree.isEmpty() {
+		sc.addSlab(slabClassIndex, sc.chunkSize, s.slabSize, s.slabMagic)
+	}
+	return sc.chunkMem(sc.popFreeChunk())
+}
+
+func (sc *slabClass) addSlab(slabClassIndex, chunkSize, slabSize int, slabMagic int32) {
+	chunksPerSlab := slabSize / chunkSize
+	slabIndex := len(sc.slabs)
+	slab := &slab{
+		// Re-multiplying to avoid any extra fractional chunk memory.
+		memory: make([]byte, (chunkSize*chunksPerSlab)+SLAB_MEMORY_FOOTER_LEN),
+		chunks: make([]chunk, chunksPerSlab),
+	}
+	footer := slab.memory[len(slab.memory)-SLAB_MEMORY_FOOTER_LEN:]
+	binary.BigEndian.PutUint32(footer[0:4], uint32(slabClassIndex))
+	binary.BigEndian.PutUint32(footer[4:8], uint32(slabIndex))
+	binary.BigEndian.PutUint32(footer[8:12], uint32(slabMagic))
+	sc.slabs = append(sc.slabs, slab)
+	for i := 0; i < len(slab.chunks); i++ {
+		c := &(slab.chunks[i])
+		c.self.slabIndex = slabIndex
+		c.self.chunkIndex = i
+		sc.pushFreeChunk(c)
+	}
+}
+
+func (sc *slabClass) pushFreeChunk(c *chunk) {
+	if c.refs != 0 {
+		panic(fmt.Sprintf("pushFreeChunk() when non-zero refs: %v", c.refs))
+	}
+	c.next = sc.chunkFree
+	sc.chunkFree = c.self
+}
+
+func (sc *slabClass) popFreeChunk() *chunk {
+	if sc.chunkFree.isEmpty() {
+		panic("popFreeChunk() when chunkFree is empty")
+	}
+	c := sc.chunk(sc.chunkFree)
+	if c.refs != 0 {
+		panic(fmt.Sprintf("popFreeChunk() when non-zero refs: %v", c.refs))
+	}
+	c.refs = 1
+	sc.chunkFree = c.next
+	c.next = empty_chunkLoc
+	return c
+}
+
+func (sc *slabClass) chunk(cl chunkLoc) *chunk {
+	if cl.isEmpty() {
+		return nil
+	}
+	return &(sc.slabs[cl.slabIndex].chunks[cl.chunkIndex])
+}
+
+func (sc *slabClass) chunkMem(c *chunk) []byte {
+	if c == nil || c.self.isEmpty() {
+		return nil
+	}
+	beg := sc.chunkSize * c.self.chunkIndex
+	return sc.slabs[c.self.slabIndex].memory[beg : beg+sc.chunkSize]
+}
+
+// Determine the slabClass & chunk for a buf []byte.
+func (s *slabArena) bufContainer(buf []byte) (*slabClass, *chunk) {
+	rest := buf[:cap(buf)]
+	footerDistance := len(rest)-SLAB_MEMORY_FOOTER_LEN
+	footer := rest[footerDistance:]
+	slabClassIndex := binary.BigEndian.Uint32(footer[0:4])
+	slabIndex := binary.BigEndian.Uint32(footer[4:8])
+	slabMagic := binary.BigEndian.Uint32(footer[8:12])
+	if slabMagic != uint32(s.slabMagic) {
+		panic(fmt.Sprintf("wrong slabMagic, buf not from this arena: %v vs %v",
+			slabMagic, uint32(s.slabMagic)))
+	}
+	sc := &(s.slabClasses[slabClassIndex])
+	slab := sc.slabs[slabIndex]
+	chunkIndex := len(slab.chunks) - (footerDistance / sc.chunkSize)
+	return sc, &(slab.chunks[chunkIndex])
+}
