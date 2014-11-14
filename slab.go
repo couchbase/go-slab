a079c8697226722bf4a25a57b40a1ba154d6aba5
Steve Yen <steve.yen@gmail.com>
2014-11-13 18:36:08 -0800

    Added package comment.

--- slab.go
+++ slab.go
@@ -9,6 +9,7 @@
 //  express or implied. See the License for the specific language
 //  governing permissions and limitations under the License.
 
+// Package slab provides a slab allocator for golang.
 package slab
 
 import (
